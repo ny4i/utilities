62628195f611b28d3210087b54a24c095ba941b8
Thomas Schaefer <toms@Mac-mini.local>
2020-01-06 17:55:56 -0500

    Initial commit

---
+++ CheckCabExchanges.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+# QSO: 14096 RY 2020-01-04 1800 W4TA          599 FL     K1SM          599 MA     0 
+my %info;
+
+while (<>) {
+   my $line = $_;
+   if ($line =~ /^QSO/) {
+      #print $_;
+      my ($header,$freq,$mode,$qsoDate,$qsoTime,$myCall,$myExch1, $myExch2,$call,$exch1,$exch2,$radio) = split /\s{1,}/, $line;
+      if ($exch2 =~ /^[+-]?\d+$/ ) {
+         #print "DX?\t$call\t$exch2\n";
+      } else {
+         # Not a number so check if valid abbreviation
+         if ($exch2 =~ /^(A[KLRZ]|C[AOT]|D[CE]|FL|GA|HI|I[ADLN]|K[SY]|LA|M[ADEINOST]|N[CDEHJMVY]|O[HKR]|P[AR]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY]|ON|AB|SK|BC|QC|NS|MB)$/) {
+         } else {
+            print "$call\t $exch2 not valid\n";
+         }
+         # Add to hash
+         if (exists($info{$call})) {
+            if ($info{$call} ne $exch2){
+               print "Prior call record for $call had exchange of $info{$call} - current exchange = $exch2\n";
+            }
+         }
+         else {
+            $info{$call} = $exch2;
+         }
+      }
+   }
+}
